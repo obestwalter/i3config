e9e4dd4d512caabe65be1a55b32de655a435bf7a
Oliver Bestwalter <oliver@bestwalter.de>
2013-09-22 18:53:11 +0200

    sleep shorter

--- bin/start_xautolock.sh
+++ bin/start_xautolock.sh
@@ -1,23 +1,6 @@
 #!/bin/bash -
-#
-#vim:foldmethod=marker commentstring=\ ##%s
-#===============================================================================
-#
-#          FILE:  start_xautolock.sh
-#
-#         USAGE:  ./start_xautolock.sh
-#
-#   DESCRIPTION:  starts xautolock
-#
-#       OPTIONS:  ---
-#  REQUIREMENTS:  ---
-#          BUGS:  ---
-#         NOTES:  ---
-#        AUTHOR: Matthias Ruf (mruf), matthias.ruf@avira.com
-#  ORGANIZATION: Avira Operations GmbH & Co. KG
-#       CREATED: 2012-04-17 10:54:13
-#      REVISION:  ---
-#===============================================================================
 
 set -o nounset                              # Treat unset variables as an error
-xautolock -time 5 -notifier 'echo "screen     lock  " | dzen2 -p 10 -y 512 -bg "#770000"' -notify 3 -locker "i3lock -i ~/chill/wallpapers/cassini_derelict.png" &
+xautolock -time 5 -corners +--- -cornerdelay 5 -notifier 'notify-send --expire-time 2 "about to lock the screen ..."' -notify 3 -locker "i3lock -i ~/chill/wallpapers/cassini_derelict.png" &
+
+#xautolock -time 5 -notifier 'echo "screen     lock  " | dzen2 -p 10 -y 512 -bg "#770000"' -notify 3 -locker "i3lock -i ~/chill/wallpapers/cassini_derelict.png" &
