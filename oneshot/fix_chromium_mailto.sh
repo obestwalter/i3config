eaa6f11cb96016a3fcd1b42d3d575896af19d276
Oliver Bestwalter <oliver@bestwalter.de>
2013-09-22 19:54:26 +0200

    mail hack

--- oneshot/fix_chromium_mailto.sh
+++ oneshot/fix_chromium_mailto.sh
@@ -3,6 +3,8 @@
 # Make chromium-browser open mail with evolution
 # Just a hack - will likely break if xdg-utils are updated
 # Figure out a different method tills then
+# envvar option seems not to work either
+# https://bugs.freedesktop.org/show_bug.cgi?id=6615
 
 mkdir -p ~/.gconf/desktop/gnome/url-handlers/mailto
 gconftool-2 --set /desktop/gnome/url-handlers/mailto/command -t string 'evolution %s'
