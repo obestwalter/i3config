f724e6886b768a0d1d9b3f75b7766846c0977761
Oliver Bestwalter <oliver.bestwalter@avira.com>
2020-03-19 13:21:07 +0100

    small tweaks

--- bin/screenshot-and-gwenview.sh
+++ bin/screenshot-and-gwenview.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-filename="scrot-$(date +"%y-%m-%d-%T").png"
+filename="scrot-$(date +"%y-%m-%d-%H-%M-%S").png"
 scrot "$HOME/$filename"
 gwenview "$HOME/$filename"
---
+++ py3status/README.md
@@ -0,0 +1,3 @@
+# py3status
+
+https://gist.github.com/ultrabug/ee216a38a0152bce4b2cef73ef358603
