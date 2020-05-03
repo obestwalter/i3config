784334c52cbc82978dc7bbaf049fa028361ea3c7
Oliver Bestwalter <oliver.bestwalter@avira.com>
2020-05-03 15:00:53 +0200

    don't react to unclutter state, shorter notification

--- bin/toggle-touchpad.sh
+++ bin/toggle-touchpad.sh
@@ -1,22 +1,23 @@
 #!/bin/bash
 
-#set -x
+# set -x
 
 declare -i ID
 ID=$(xinput list | grep -Eio '(touchpad|glidepoint)\s*id\=[0-9]{1,2}' | grep -Eo '[0-9]{1,2}')
 declare -i STATE
 STATE=$(xinput list-props "$ID"|grep 'Device Enabled'|awk '{print $4}')
-UNCLUTTER_PID=$(pgrep unclutter)
+#UNCLUTTER_PID=$(pgrep unclutter)
 if [ "$STATE" -eq 1 ]; then
     xinput disable "$ID"
-    notify-send -a 'CENTRAL-CONTROL-PROGRAM' 'touchpad disabled'
-    # shellcheck disable=SC2046
-    if [ "$UNCLUTTER_PID" = "" ]; then
+    notify-send -t 1000 -a 'CENTRAL-CONTROL-PROGRAM' 'touchpad disabled'
+#    if [ "$UNCLUTTER_PID" = "" ]; then
+#      xdotool mousemove 5000 5000
+#    fi
+      # send outside of viewport => pops up again middle/lower third of screen then
       xdotool mousemove 5000 5000
-    fi
 else
     xinput enable "$ID"
-    notify-send -a 'CENTRAL-CONTROL-PROGRAM' 'touchpad enabled'
+    notify-send -t 1000 -a 'CENTRAL-CONTROL-PROGRAM' 'touchpad enabled'
 #    if [ "$UNCLUTTER_PID" = "" ]; then
 #      xdotool mousemove 1000 500
 #    fi
