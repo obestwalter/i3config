b15b56d7c80fbe2fdae52cabb3e68315cf6fbb87
Oliver Bestwalter <oliver@bestwalter.de>
2025-06-26 08:09:45 +0200

    improve ccp

--- bin/i3configger-wrapper.sh
+++ bin/i3configger-wrapper.sh
@@ -6,11 +6,11 @@ PID=$(pgrep i3configger)
 if [ "$PID" == "" ]; then
   i3configger --watch -v &
   PID=$(pgrep i3configger)
-  notify-send -a 'CENTRAL-CONTROL-PROGRAM' "i3configger started @$PID"
+  notify-send -a "$I3CONFIGGER" "started @$PID"
 fi
 
 if ! $I3CONFIGGER "$1"; then
-  notify-send --urgency=critical -a 'CENTRAL-CONTROL-PROGRAM' "$I3CONFIGGER $1 failed"
+  notify-send --urgency=critical -a "$I3CONFIGGER" "failed: $1"
 else
-  notify-send --urgency=low -a 'CENTRAL-CONTROL-PROGRAM' "$I3CONFIGGER $1"
+  notify-send --urgency=low -a "$I3CONFIGGER" "done: $1"
 fi
